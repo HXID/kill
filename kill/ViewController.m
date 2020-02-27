6bd72e63f4d83be750b110b209e6b4b391fb07be
test <zjp15083791743@163.com>
2020-02-28 01:13:17 +0800

    1223

---
+++ kill/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  kill
+//
+//  Created by test on 2020/2/28.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    101
+}
+
+
+@end
